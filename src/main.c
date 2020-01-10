6f385ee2335627e5122e5e8a243807c8e8c95d02
nelsontss <nelsontss98@gmail.com>
2020-01-10 09:30:48 +0000

    fix

--- src/main.c
+++ src/main.c
@@ -345,37 +345,13 @@ static int xmp_write(const char *path, const char *buf, size_t size,
 	if (fd == -1)
 		return -errno;
 	
-	FILE* fp;
-	bool newFile;
-	fp = fopen(path, "r");
-	if (fp!=NULL ) {
-		newFile = false;	
-	} else {
-		newFile = true;	
-	} 
-
-	if(!newFile){
-		char *inital_text = malloc((size + 1) * size);
-		fseek(fp, offset, SEEK_SET);
-		fread(inital_text, size, 1, fp);
-		fseek(fp, 0, SEEK_SET);
-		double initial_entropy = calculate_entropy(inital_text, strlen(inital_text));	
-		double final_entropy = calculate_entropy(buf, size);
 	
-		if (final_entropy > 6.0 && final_entropy - initial_entropy < 2) {
-			res = pwrite(fd, buf, size, offset);
-		}else{
-			res = -1;
-		}
-	}else{
-
+	double final_entropy = calculate_entropy(buf, size);
+	printf("Final entropy: %f\n", final_entropy);
+	if (final_entropy < 6.0) {
 		res = pwrite(fd, buf, size, offset);
-
-		if (res == -1)
-		res = -errno;
-
-		if(fi == NULL)
-			close(fd);	
+	}else{
+		res = -1;
 	}
 	
 	return res;
